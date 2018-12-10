e51af501adfc08b5a1309f427cb56e51c2e5a77c
Feureau-PC <feureau@gmail.com>
2018-12-10 13:54:35 +0700

    Shmup and sidescroller

--- MobileShadow_Low/Intermediate/Build/Android/MobileShadow_Low/Development/UELinkerFixups/PCH.UELinkerFixups.h
+++
@@ -1,2 +0,0 @@
-// PCH for D:\Github\BlueprintRecipeRepository\MobileShadow_Low\Intermediate\Build\Android\UE4\Development\UELinkerFixupsName.h
-#include "D:\Github\BlueprintRecipeRepository\MobileShadow_Low\Intermediate\Build\Android\UE4\Development\UELinkerFixupsName.h"
